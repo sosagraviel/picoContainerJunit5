3bfcc1b840cb10ac9d83a81c4fce40c12167dacc
gravielsosa <sosagraviel@gmail.com>
2024-10-08 17:23:47 -0300

    imprudent

--- basicinter/src/test/java/TestNGFramework/CrossBrowserTesting.java
+++ basicinter/src/test/java/TestNGFramework/CrossBrowserTesting.java
@@ -6,45 +6,51 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class CrossBrowserTesting {
-    String url = "http://www.automationpractice.pl/";
-    WebDriver driver;
+	String url = "http://www.automationpractice.pl/";
+	WebDriver driver;
 
-    @BeforeTest
-    @Parameters("navegador")
-    public void setUp(@Optional("chrome")String navegador) {
-        if(navegador.equalsIgnoreCase("chrome")) {
-            System.out.println("Inicio de pruebas en Chrome");
-            driver = new ChromeDriver();
+	@BeforeTest
+	@Parameters("navegador")
+	public void setUp(@Optional("firefox") String navegador) {
+		if (navegador.equalsIgnoreCase("chrome")) {
+			System.out.println("Inicio de pruebas en Chrome");
+			driver = new ChromeDriver();
 
-        }
-        if(navegador.equalsIgnoreCase("firefox")) {
-            System.out.println("Inicio de pruebas en Firefox");
-            driver = new FirefoxDriver();
+		}
+		if (navegador.equalsIgnoreCase("firefox")) {
+			System.out.println("Inicio de pruebas en Firefox");
+			driver = new FirefoxDriver();
 
-        }
-        driver.get(url);
-        driver.manage().window().maximize();
+		}
+		driver.get(url);
+		driver.manage().window().maximize();
 
-    }
-    @Test
-    public void searchItem() throws InterruptedException {
-        WebElement searchItemInput = driver.findElement((By.id("search_query_top")));
-        searchItemInput.sendKeys("t-shirts");
+	}
 
-        WebElement searchButton = driver.findElement(By.name("submit_search"));
-        searchButton.click();
+	@Test
+	public void searchItem() {
+		WebElement searchItemInput = driver.findElement((By.id("search_query_top")));
+		searchItemInput.sendKeys("t-shirts");
 
-        WebElement searchResult = WaitUtilities.waitForVisibility(driver, 10, driver.findElement(By.tagName("h1")));
-        System.out.println(searchResult.getText());
+		WebElement searchButton = driver.findElement(By.name("submit_search"));
+		searchButton.click();
 
-    }
-    @AfterTest
-    public void tearDown() {
-        System.out.println("Fin de pruebas");
-        driver.close();
+		WebElement searchResult = WaitUtilities.waitForVisibility(driver, 10, driver.findElement(By.tagName("h1")));
+		System.out.println(searchResult.getText());
 
-    }
+	}
+
+	@AfterTest
+	public void tearDown() {
+		System.out.println("Fin de pruebas");
+		driver.close();
+
+	}
 }
