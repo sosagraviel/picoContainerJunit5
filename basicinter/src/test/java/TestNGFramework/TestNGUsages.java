9fcb6fed2000a114ce22cee18a2558ce18cd6942
gravielsosa <sosagraviel@gmail.com>
2024-11-07 10:11:01 -0300

    class 3

--- basicinter/src/test/java/TestNGFramework/TestNGUsages.java
+++ basicinter/src/test/java/TestNGFramework/TestNGUsages.java
@@ -11,14 +11,14 @@ public class TestNGUsages {
         if (i % 2 != 0) {
             condition = false;
         }
-        Assert.assertTrue(condition);
+        Assert.assertTrue("La condición es falsa, el test falló.",condition);
         System.out.println("Test finished OK");
     }
     @Test(description = "Test numero 2", dependsOnMethods = "testTres")
     public void testDos() {
         System.out.println("TEST DOS");
     }
-    @Test(description = "Test numero 3", priority = 1)
+    @Test(description = "Test numero 3", priority = 2)
     public void testTres() {
         System.out.println("TEST TRES");
     }
