3a1417070cf066ab7926749d25bbfc3d9c907b27
gravielsosa <sosagraviel@gmail.com>
2024-12-19 14:56:29 -0300

    update

--- cucumber-archetype-parallel-advance/src/test/java/workshopcucumberadvance/Test/StepsDef/Runner/RunCucumberTest.java
+++ cucumber-archetype-parallel-advance/src/test/java/workshopcucumberadvance/Test/StepsDef/Runner/RunCucumberTest.java
@@ -2,6 +2,7 @@ package workshopcucumberadvance.Test.StepsDef.Runner;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import static io.cucumber.junit.platform.engine.Constants.PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME;
@@ -11,6 +12,7 @@ import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_N
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("workshopcucumberadvance")
+@SelectClasspathResource("workshopcucumberadvance")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 //this parameter will add the allure report as an additional
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
